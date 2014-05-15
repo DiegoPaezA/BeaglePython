2a60285c172068a8a737933294bb6d153661dba5
DiegoPaez <diripar8@gmail.com>
2014-05-14 23:20:44 -0300

    adc update

--- ADCi2c/ADS1115/ecgREad.py
+++ ADCi2c/ADS1115/ecgREad.py
@@ -15,16 +15,8 @@ signal.signal(signal.SIGINT, signal_handler)
 ADS1115 = 0x01	# 16-bit ADC
 
 # Select the gain
-# gain = 6144  # +/- 6.144V
-gain = 4096  # +/- 4.096V
-#gain = 2048  # +/- 2.048V
-#gain = 1024  # +/- 1.024V
-# gain = 512   # +/- 0.512V
-# gain = 256   # +/- 0.256V
-
-
-#sps = 250  # 250 samples per second
-#sps = 475  # 475 samples per second
+#gain = 4096  # +/- 4.096V
+gain = 6144  # +/- 6.144V
 sps = 860  # 860 samples per second
 
 # Initialise the ADC using the default mode (use default I2C address)
@@ -36,7 +28,7 @@ stop_time = int(raw_input("Ingresar Tiempo de la Prueba en Segundos : "))
 print "El tiempo es : ", stop_time , "Segundos "
 start_time = time.time()
 loop_time=0
-
+i = 0
 while (loop_time<=stop_time):
             # Read channel 0 in single-ended mode using the settings above
         volts = adc.readADCSingleEnded(0, gain, sps) / 1000
@@ -44,11 +36,16 @@ while (loop_time<=stop_time):
         
         temp_time = time.time()
         loop_time = temp_time - start_time
+        
+        if loop_time> 1+i:
+                print "Tiempo Actual: ", int(loop_time) , "Seg"
+                i +=1
+                
 
 print len(ecgRead)
 np.savetxt('ecg.txt', ecgRead, fmt='%10.2f')
 pg.plot(ecgRead)
 
-print "Duracion de la prueba : ", loop_time , "Segundos "
+print "Duracion de la prueba : ", int(loop_time) , "Segundos "
     
 print "Prueba Finalizada"    
