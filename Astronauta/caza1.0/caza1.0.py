1509a24a8376c13d5c2de98b506e8e7c208ac22e
diegopaez <diripar8@gmail.com>
2015-03-02 07:04:42 +0000

    problem when thread is stop and still is saving data

--- Astronauta/caza1.0/caza1.0.py
+++ Astronauta/caza1.0/caza1.0.py
@@ -12,80 +12,53 @@ class MicroGravedad(QtCore.QObject):
     def __init__(self):
         super(MicroGravedad, self).__init__()
         
-        # Crear Timer read Imu
-        self.timerimu = QtCore.QTimer(self)
-        self.timerimu.timeout.connect(self.readImu)
-        self.timerimu.setInterval(20) #50Hz -> 20ms
+        # Thread Imus Temperatura
+        self.threadimu = QtCore.QThread()
+        self.workerimu = WorkerImu()
+        self.workerimu.moveToThread(self.threadimu)
+        self.threadimu.started.connect(self.workerimu.loop)
         
-        #
         # adding by emitting signal in different thread
-        self.thread = QtCore.QThread()
-        self.worker = Worker()
-        self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.loop)
-        
-        GPIO.add_event_detect("P9_12", GPIO.RISING,callback=self.start, bouncetime=100)
-        GPIO.add_event_detect("P9_14", GPIO.RISING,callback=self.stop, bouncetime=100)
-        
+        self.threadadc = QtCore.QThread()
+        self.workeradc = WorkerADC()
+        self.workeradc.moveToThread(self.threadadc)
+        self.threadadc.started.connect(self.workeradc.loop)
+
         self.dataread = raw_input("Enter Session Name: ")
         self.crearDir() ## crear directorio
         print "Push Start button"
         
-        #Clase leer aceleracion
-        self.Imu=ReadAccel()
-        self.accel = open("accel.txt", "w")
-        self.accel.write("Ax;Ay;Az;")
-        self.accel.write("\n")
-        self.ite = 0
+        GPIO.add_event_detect("P9_12", GPIO.RISING,callback=self.start, bouncetime=100)
+        GPIO.add_event_detect("P9_14", GPIO.RISING,callback=self.stop, bouncetime=100)
         
-        #self.timerimu.start()
 
     def start(self,isr): 
         #-----------------------------------
         print "Start Capture"
-        self.thread.start() # Worker Thread
+        #self.threadadc.start() # Worker Thread
+        self.threadimu.start() # Worker Thread
         GPIO.remove_event_detect("P9_12")
         #-----------------------------------
         return
     def stop(self,isr): 
         #------------------
-        if self.thread.isRunning() == True:
-            self.worker.stop()
-            self.thread.quit()
-            self.thread.wait()
+        if self.threadadc.isRunning() == True:
+            self.workeradc.stop()
+            self.threadadc.quit()
+            self.threadadc.terminate()
             print "stop Thread"
             #------------------
+        if self.threadimu.isRunning() == True:
+            self.workerimu.stop()
+            self.threadimu.quit()
+            self.threadimu.terminate()
+            print "stop Thread"
+            #------------------    
         print "stop program"
-        #---------------------------------------
-        if self.timerimu.isActive() == True:
-            self.timerimu.stop()
-        self.accel.close() #close accel file
-        #---------------------------------------
-        GPIO.remove_event_detect("P9_14")
         QtCore.QCoreApplication.exit(0) # exit app
         return
-    
- 
-    def readImu(self):
-        print "--> ok"
-        fax, fay, faz= self.Imu.readAccel()
-        ax = "%.4f" % round(fax,4)
-        ay = "%.4f" % round(fay,4)
-        az = "%.4f" % round(faz,4)
-        print "acel x: ", ax, " acel y: ", ay, " acel z: ", az
-        self.accel.write(str(ax))
-        self.accel.write(";")
-        self.accel.write(str(ay))
-        self.accel.write(";")
-        self.accel.write(str(az))
-        self.accel.write(";")
-        self.accel.write("\n")
-        
-        self.ite += 1
-        if self.ite == 50:
-            print "Read Temperatura"
-            self.ite = 0
-
+    def test(self):
+        print "its working"
     def crearDir(self):
         self.directorioOriginal = os.getcwd()
         carpeta = "caza1.0/" +  str(self.dataread)
@@ -94,20 +67,54 @@ class MicroGravedad(QtCore.QObject):
         if not os.path.isdir(directorio):
             os.mkdir(directorio)
         os.chdir(directorio)
+
+class WorkerImu(QtCore.QObject):
+    def readImu(self):
+        fax, fay, faz= self.Imu.readAccel()
+        ax = "%.4f" % round(fax,4)
+        ay = "%.4f" % round(fay,4)
+        az = "%.4f" % round(faz,4)
+        #print "acel x: ", ax, " acel y: ", ay, " acel z: ", az
+        self.data[0] = ax
+        self.data[2] = ay
+        self.data[4] = az
+        for i in self.data:
+            self.accel.write(str(i))
+        self.accel.write("\n")
         
-class Worker(QtCore.QObject):
-    def do_stuff_timer(self):
-        print "Read Adc"
-
+        self.ite += 1
+        if self.ite == 50:
+            print "Read Temperatura"
+            self.ite = 0
     def stop(self):
-        self._exit = True
+        self.accel.close() #close accel file
         self.timer.stop()
-
+        self._exit = True
     def loop(self):
         self.timer = QtCore.QTimer()
         self.timer.setSingleShot(False)
-        self.timer.timeout.connect(self.do_stuff_timer)
+        self.timer.timeout.connect(self.readImu)
+        self.timer.start(20)
+        #Clase leer aceleracion
+        self.Imu=ReadAccel()
+        self.accel = open("accel.txt", "w")
+        self.accel.write("Ax;Ay;Az;")
+        self.accel.write("\n")
+        self.data = [0,';',0,';',0,';']
+        self.ite = 0
+        
+class WorkerADC(QtCore.QObject):
+    def readEcg(self):
+        print "Read Adc"
+    def stop(self):
+        self.timer.stop()
+        self._exit = True
+    def loop(self):
+        self.timer = QtCore.QTimer()
+        self.timer.setSingleShot(False)
+        self.timer.timeout.connect(self.readEcg)
         self.timer.start(200)
+        
 if __name__ == "__main__":
     
     app = QtCore.QCoreApplication(sys.argv)
