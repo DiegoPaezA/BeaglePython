638151cb1ac5714fe2bbffd618614d6a86a3435f
diegopaez <diripar8@gmail.com>
2015-05-01 00:11:29 -0300

    nueva version con gestion de directorios y mejores nombres

---
+++ control4Deportivo/control.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# Created: Fri Nov 28 14:43:23 2014
+#
+# Diego R. Paez A.
+# Control Deportivo, Registro VFC e EMG
+
+
+
+from PyQt4 import QtCore, QtGui
+from ui.home import MainWindow
+ 
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
