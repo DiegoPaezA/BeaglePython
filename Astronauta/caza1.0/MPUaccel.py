76f2d1d7e2399df27095920febc80834ee66bdc1
diegopaez <diripar8@gmail.com>
2016-01-24 20:17:53 +0000

    update

--- Astronauta/caza1.0/MPUaccel.py
+++ Astronauta/caza1.0/MPUaccel.py
@@ -15,10 +15,10 @@ class ReadAccel :
     MPU6050_RA_TEMP_OUT_H= 		    0x41
     MPU6050_RA_ACCEL_CONFIG= 		0x1C
     
-    def __init__(self, address=0x68,i2c_bus = 2, Arange =2):
+    def __init__(self, address=0x68,i2c_bus = 1, Arange =2):
         print "start--config MPU6050" 
         self.bus = smbus.SMBus(i2c_bus)    # or bus = smbus.SMBus(1) for Revision 2 boards
-        self.address = 0x68       # This is the address value read via the i2cdetect command
+        self.address = address       # This is the address value read via the i2cdetect command
         self.bus.write_byte_data(self.address, self.power_mgmt_1, 0) # Now wake the 6050 up as it starts in sleep mode
         self.setRange(Arange)
         
--- blink/blink2.py
+++ blink/blink2.py
@@ -1,12 +1,25 @@
 from bbio import *
+import Adafruit_BBIO.GPIO as GPIO
 
 LED = GPIO0_4
-
+off = 0
 def setup():
   pinMode(LED, OUTPUT)
-
+  GPIO.setup("P9_24", GPIO.IN)
+  GPIO.add_event_detect("P9_24", GPIO.RISING,callback=start, bouncetime=100)
+def start(isr):
+  global off
+  #-----------------------------------
+  print "Start Capture"
+  GPIO.remove_event_detect("P9_24")
+  #-----------------------------------
+  off =1
+  return
 def loop():
+  global off
   toggle(LED)
   delay(500)
+  if off == 1:
+    exit()
 
 run(setup, loop)
